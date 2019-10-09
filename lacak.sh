9a9ef8dc09a08e0798d9969c3b89abee592ef189
R3f4ll <53784408+R3f4ll@users.noreply.github.com>
2019-10-09 17:11:19 +0700

    Add files via upload

---
+++ lacak.sh
@@ -0,0 +1,39 @@
+#mau recode?
+#pm admin dulu ajg!
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo                         pur='\033[35;1m' #purple                    cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+mer='\033[41;97m' #Tepi
+st='\033[0m' #Stop
+R='\x1b[1;31m'
+G='\x1b[1;32m'
+B='\x1b[1;34m'
+Y='\x1b[1;33m'
+C='\x1b[1;36m'
+D='\x1b[0m'
+
+clear
+figlet Welcome | lolcat
+echo $me"["$ku"!"$me"]"$i"Lu Buriq Tq :v"
+echo $cy"<===============================>" | lolcat
+echo $cy"•Author  : Mr.R3f4ll"
+echo $ku"•Team    : Up_Cyber_Army"
+echo $me"•Instagram :@r3f4ll_ "
+echo $cy"<===============================>" | lolcat
+echo $i"["$me"!"$i"]""Sedang Loading..."
+sleep 8
+echo $i"paketan dah nyala?"
+echo $me"kalau udah okh"
+       url=http://ip-api.com/
+       read -p"Masukkan Ip Target Ajg : " pil;
+       echo $ku"Mengecek $pil..."
+       sleep 5
+       echo $ku"Melacak $pil..."
+       sleep 15
+       echo $i"Lokasi DiTemukan!"
+       curl -s $url$pil
+       echo $me"Kelacak Kan Akowkw"
+       exit
